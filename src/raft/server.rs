b60918833d112782ea30972c1e6751069eee4c5d
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2021-01-16 01:15:17 +1100

    grrr

--- src/raft/server.rs
+++ src/raft/server.rs
@@ -1,12 +1,16 @@
 use actix::prelude::*;
 
-use actix_rt::net::TcpStream;
+use actix_rt::net::{TcpStream};
 use actix_server::Server;
 use actix_service::pipeline_factory;
 use std::time::Duration;
 use uuid::Uuid;
-
+use std::sync::Arc;
+use serde::{Serialize, Deserialize};
+use bytes::BytesMut;
+use tokio_io::AsyncRead;
 use crate::raft::state;
+
 
 #[derive(Message)]
 #[rtype(result="()")]
@@ -17,6 +21,7 @@ pub struct Ping;
 #[rtype(result="()")]
 pub struct Timeout;
 
+#[derive(Serialize, Deserialize)]
 pub enum Msg {
     Msg,
     HeartBeat
@@ -24,12 +29,13 @@ pub enum Msg {
 
 pub struct TcpClient {
     stream: TcpStream,
-    timer: Option<SpawnHandle>
+    timer: Option<SpawnHandle>,
+    addr: Arc<Addr<state::Raft>>
 }
 
 impl TcpClient {
-    fn new(stream: TcpStream) -> TcpClient {
-        TcpClient{stream, timer: None}
+    fn new(stream: TcpStream, addr: Arc<Addr<state::Raft>>) -> TcpClient {
+        TcpClient{stream, timer: None, addr}
     }
 }
 
@@ -71,24 +77,34 @@ pub struct TcpServer {
 
 }
 
+#[derive(Clone)]
+pub struct Passable {
+    addr: Arc<Addr<state::Raft>>
+}
+
 async fn start() {
     let addr = ("127.0.0.1", 8080);
-    // let raft = state::Raft::default(Uuid::new())
-    Server::build()
+    let raft =  Arc::new(state::Raft::default(Uuid::new_v4()).start());
+    let x = Server::build()
         .bind("raft", addr, move || {
-            pipeline_factory(move |mut stream: TcpStream| {
+            let raft = Arc::clone(&raft);
+            pipeline_factory( move |mut stream: TcpStream| {
+                let raft = Arc::clone(&raft);
 
                 async move {
-                    
-                    let tcp_client = TcpClient::new(stream);
+                    let tcp_client = TcpClient::new(stream, raft).start();
 
+                    let mut size: u64 = 0;
+                    let mut buf = BytesMut::new();
+                    
+                    loop {
+                        
+                    }
                     // send data down service pipeline
                     let res: Result<(), ()> = Ok(());
 
                     res
                 }
             })
-        }).map_err(|err| {
-
         });
 }
