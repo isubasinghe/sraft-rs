92710981943c3f45c7ed2f449b2a2bce6db9543f
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2021-01-15 22:42:27 +1100

    feat: added server

--- src/raft/server.rs
+++ src/raft/server.rs
@@ -1,6 +1,91 @@
-use std::sync::atomic::{AtomicUsize, Ordering::Relaxed};
-use std::sync::{mpsc, Arc};
-use std::{net, thread, time};
+use actix::prelude::*;
 
+use actix_rt::net::TcpStream;
 use actix_server::Server;
-use actix_service::fn_service;
+use actix_service::pipeline_factory;
+use std::time::Duration;
+
+#[derive(Message)]
+#[rtype(result="()")]
+pub struct Ping;
+
+
+#[derive(Message)]
+#[rtype(result="()")]
+pub struct Timeout;
+
+pub enum Msg {
+    Msg,
+    HeartBeat
+}
+
+pub struct TcpClient {
+    stream: TcpStream,
+    timer: Option<SpawnHandle>
+}
+
+impl TcpClient {
+    fn new(stream: TcpStream) -> TcpClient {
+        TcpClient{stream, timer: None}
+    }
+}
+
+
+impl Actor for TcpClient {
+    type Context = Context<Self>;
+
+    fn started(&mut self, ctx: &mut Context<Self>) {
+        self.timer = Some(ctx.run_later(Duration::from_secs(1), |_, ctx| {
+            ctx.address().do_send(Timeout);
+        }));
+    }
+}
+
+impl Handler<Ping> for TcpClient {
+    type Result = ();
+
+    fn handle(&mut self, msg: Ping, ctx: &mut Context<Self>) -> Self::Result {
+        match self.timer {
+            Some(handle) => {ctx.cancel_future(handle);}
+            None => {}
+        }
+        self.timer = Some(ctx.run_later(Duration::from_secs(1), |_, ctx| {
+            ctx.address().do_send(Timeout);
+        }));
+        ()
+    }
+}
+
+impl Handler<Timeout> for TcpClient {
+    type Result = ();
+    fn handle(&mut self, msg: Timeout, ctx: &mut Context<Self>) -> Self::Result {
+
+        ()
+    }
+}
+
+pub struct TcpServer {
+
+}
+
+async fn start() {
+    let addr = ("127.0.0.1", 8080);
+
+    Server::build()
+        .bind("raft", addr, move || {
+            pipeline_factory(move |mut stream: TcpStream| {
+
+                async move {
+                    
+                    let tcp_client = TcpClient::new(stream);
+
+                    // send data down service pipeline
+                    let res: Result<(), ()> = Ok(());
+
+                    res
+                }
+            })
+        }).map_err(|err| {
+
+        });
+}
