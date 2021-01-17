bbade59bcec968d7c405315dfbd9bbb285c6701a
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2021-01-17 21:29:55 +1100

    feat: added some tcp handling stuff

--- src/raft/client.rs
+++ src/raft/client.rs
@@ -1,10 +1,12 @@
 use tokio::net::TcpStream;
-use tokio::io::AsyncWriteExt;
-use std::error::Error;
 use serde::{Deserialize, Serialize};
 use actix::prelude::*;
-
+use tokio_util::codec::{FramedWrite, FramedRead, LengthDelimitedCodec};
+use tokio_serde::formats::*;
+use tokio_serde::*;
+use serde_json::*;
 use std::rc::Rc;
+use futures::prelude::*;
 
 #[derive(Deserialize, Serialize)]
 pub struct Data {
@@ -24,12 +26,12 @@ pub enum Msg {
 pub struct NewHost(String);
 
 pub struct Client {
-    client: TcpStream,
+    client: Framed<FramedWrite<TcpStream, LengthDelimitedCodec>, Msg, Msg, Bincode<Msg, Msg>>,
     manager: Addr<ClientManager>
 }
 
 impl Client {
-    fn new(client: TcpStream, manager: Addr<ClientManager>) -> Client {
+    fn new(client: Framed<FramedWrite<TcpStream, LengthDelimitedCodec>, Msg, Msg, Bincode<Msg, Msg>>, manager: Addr<ClientManager>) -> Client {
         Client{client, manager}
     }
 }
@@ -58,8 +60,28 @@ impl Handler<NewHost> for ClientManager {
             let stream = TcpStream::connect("").await;
             match stream {
                 Ok(stream) => {
-                    // let length_delimited = FrameWrite
-                    let client = Client::new(stream, addr).start();
+                    let stream1 = stream.into_std().unwrap();
+                    let stream2 = stream1.try_clone().unwrap();
+                    
+                    let stream1_ = TcpStream::from_std(stream1).unwrap();
+                    let stream2_ = TcpStream::from_std(stream2).unwrap();
+
+                    let length_delimited = FramedWrite::new(stream1_, LengthDelimitedCodec::new());
+                    let length_delimited_ = FramedRead::new(stream2_, LengthDelimitedCodec::new());
+                    // Serialize frames with JSON
+                    let mut serialized: Framed<FramedWrite<TcpStream, LengthDelimitedCodec>, Msg, Msg, Bincode<Msg, Msg>> =
+                        SymmetricallyFramed::new(length_delimited, SymmetricalBincode::default());
+
+                    tokio::spawn(async move {
+                        let mut deserialized = tokio_serde::SymmetricallyFramed::new(
+                            length_delimited_,
+                            SymmetricalJson::<Msg>::default(),
+                        );
+
+                        while let Some(msg) = deserialized.try_next().await.unwrap() {
+                            
+                        }
+                    });
                     
                 },
                 Err(e) => {
