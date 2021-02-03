45da9beb3b9e875483b0bb8f2e35489b1d4489fa
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2021-02-03 18:28:46 +1100

    feat: got client sorta working

--- src/raft/client.rs
+++ src/raft/client.rs
@@ -2,6 +2,7 @@ use actix::prelude::*;
 use futures::executor::block_on;
 use std::time::Duration;
 use tracing::{info, warn, error};
+use std::sync::Arc;
 use crate::raft::state::Raft;
 use crate::raft::messages::*;
 use crate::raft::transformers::*;
@@ -13,13 +14,21 @@ use crate::raft::raftservice::{
 pub struct Client{
     addr: String,
     client: Option<RaftServiceClient<tonic::transport::Channel>>,
-    raft: Addr<Raft>
+    raft: Addr<Raft>,
+    handle: Arc<tokio::runtime::Runtime>,
 }
 
 impl Client {
-    pub fn new(addr: String, raft: Addr<Raft>) -> Client {
-        Client{addr, client: None, raft}
+    pub fn new(addr: String, raft: Addr<Raft>, handle: Arc<tokio::runtime::Runtime>) -> Client {
+        Client{addr, client: None, raft, handle}
     }
+}
+
+#[derive(Message)]
+#[rtype(result="()")]
+struct AddClient {
+    client: RaftServiceClient<tonic::transport::Channel>,
+    notify_msg: NotifyUUID,
 }
 
 impl Actor for Client {
@@ -27,17 +36,25 @@ impl Actor for Client {
 
     fn started(&mut self, ctx: &mut Context<Self>) {
         let addr = self.addr.clone();
-        let raft = self.raft.clone();
+        let me = ctx.address();
+        let rt = self.handle.clone();
+        let recipient = me.clone().recipient();
         let fut = async move {
             loop {
                 info!("CLIENT: Attempting connect");
-                match RaftServiceClient::connect(addr.clone()).await {
+                match rt.block_on(RaftServiceClient::connect(format!("http://{0}", addr.clone()))) {
                     Ok(mut client) => {
                         info!("CLIENT: ESTABLISHED CONNECTION");
-                        let uuid = client.get_uuid(()).await.unwrap();
+                        let uuid = match client.get_uuid(()).await {
+                            Ok(uuid) => uuid, 
+                            Err(e) => {
+                                error!("CLIENT: UNABLE TO RETRIEVE err:{0}", e);
+                                continue;
+                            }
+                        };
                         let uuid = uuid__to_uuid(uuid.into_inner()).unwrap();
-                        raft.do_send(NotifyUUID{node_id: uuid, addr: ctx.address().recipient()});
-                        return client;
+                        me.do_send(AddClient{client, notify_msg: NotifyUUID{node_id: uuid, addr: recipient}});
+                        break;
                     },
                     Err(e) => {
                         error!("CLIENT: COULD NOT ESTABLISH CONNECTION err:{0}", e);
@@ -47,7 +64,8 @@ impl Actor for Client {
                 actix::clock::delay_for(Duration::from_secs(10)).await;
             }
         };
-        self.client = Some(block_on(fut));
+        actix::spawn(fut);
+
     }
 }
 
@@ -63,7 +81,7 @@ impl Handler<NodeMsgs> for Client {
                     client.unwrap().broad_cast_msg(msg).await
                 };
 
-                let _ = block_on(fut);
+                let _ = self.handle.block_on(fut);
             },
             NodeMsgs::LogRequest(msg) => {
 
@@ -74,7 +92,7 @@ impl Handler<NodeMsgs> for Client {
                     client.unwrap().do_log_request(msg).await
                 };
 
-                let res = match block_on(fut) {
+                let res = match self.handle.block_on(fut) {
                     Ok(res) => res, 
                     Err(_) => return
                 };
@@ -91,7 +109,7 @@ impl Handler<NodeMsgs> for Client {
                 let fut = async move {
                     client.unwrap().do_vote_request(msg).await
                 };
-                let res = match block_on(fut) {
+                let res = match self.handle.block_on(fut) {
                     Ok(res) => res.into_inner(),
                     Err(_) => return
                 };
@@ -107,4 +125,15 @@ impl Handler<NodeMsgs> for Client {
         ()
     }
 
+}
+
+impl Handler<AddClient> for Client {
+
+    type Result = ();
+
+    fn handle(&mut self, msg: AddClient, ctx: &mut Context<Self>) -> Self::Result {
+        self.client = Some(msg.client);
+        self.raft.do_send(msg.notify_msg);
+        ()
+    }
 }
