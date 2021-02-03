da1781620961fb464f0fc7ac7eff413295a562cd
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2021-02-04 08:30:45 +1100

    feat: removed helloworld proto compilation

--- build.rs
+++ build.rs
@@ -4,6 +4,4 @@ fn main() {
         .type_attribute("raftservice.UUID", "#[derive(Hash, Eq)]")
         .compile(&["proto/raftservice.proto"], &["proto"])
         .unwrap();
-    
-    tonic_build::compile_protos("proto/helloworld.proto").unwrap();
 }
